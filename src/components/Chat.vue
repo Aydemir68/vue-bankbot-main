c207670c5e14bb3c13cbfe2bbab703e26ed4d848
DrMaypulla <dr.maypulla@gmail.com>
2025-01-16 21:25:23 +0300

    chat_update(add clear button)

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -4,6 +4,46 @@
       v-if="isVisible" class="scroll-btn flex align-items-center p-3 bg-gray-200 right-0 m-3 border-none active:bg-gray-300" @click="scrollToBottom">
       <i class="pi pi-chevron-down"></i>
     </Button>
+
+    <button class="bin-button" @click="clearChat">
+      <svg
+          class="bin-top"
+          viewBox="0 0 39 7"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
+        <line
+            x1="12"
+            y1="1.5"
+            x2="26.0357"
+            y2="1.5"
+            stroke="white"
+            stroke-width="3"
+        ></line>
+      </svg>
+      <svg
+          class="bin-bottom"
+          viewBox="0 0 33 39"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <mask id="path-1-inside-1_8_19" fill="white">
+          <path
+              d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
+          ></path>
+        </mask>
+        <path
+            d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+            fill="white"
+            mask="url(#path-1-inside-1_8_19)"
+        ></path>
+        <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
+        <path d="M21 6V29" stroke="white" stroke-width="4"></path>
+      </svg>
+    </button>
+
+
     <div ref="messagesContainer" class="flex flex-column h-full overflow-y-scroll mx-1">
       <div v-for="message in messages" :key="message.id">
         <div class="bg-primary-500 p-2 border-round-lg text-left" :class="{'flex user-message my-2 max-w-18rem': message.isUser, 'flex bot-message max-w-18rem': !message.isUser,}">
@@ -27,6 +67,7 @@
 <script>
 import axios from "axios";
 import {marked} from "marked";
+import {chatExists, chatGenerate, getAnswer} from "../chat_api.js";
 
 export default {
   data() {
@@ -49,64 +90,41 @@ export default {
           text: this.newMessage,
           isUser: true,
         });
+        this.scrollToBottom();
 
-        // Сохранение сообщения перед очисткой
         const userMessage = this.newMessage;
         this.newMessage = "";
-
-        // Сохранение сессии
-        this.saveSession();
-
+        const kb_id = "d5db2804-5075-4b18-9978-ec0f7d1fcbc2";
+        const tg = window.Telegram.WebApp;
         this.scrollToBottom();
 
         try {
-          this.visibleGenMessage = true;
-          this.sendButtonEnabled = true;
-          // Отправляем сообщение на сервер
-          await axios.post(
-            "http://81.94.159.227:1203/api/v1/chat/generation",
-            {
-              role: "user",
-              content: userMessage,
-            }, {
-                params: {
-                  chat_id: "1ca42f7e-0f47-4ffe-ae96-41988848997a",
-                  use_rag: true,
-                  extract_keywords: true,
-                  stream: true,}
-              }
-          );
-
-          // Получаем ответ
-          const response = await axios.get(
-            "http://81.94.159.227:1203/api/v1/chat/message/list",
-            {
-              params: { chat_id: "1ca42f7e-0f47-4ffe-ae96-41988848997a" },
+          const chat_id = await chatExists(tg.initDataUnsafe.user.id, kb_id);
+          if (chat_id) {
+            await chatGenerate(chat_id, userMessage);
+            const answer = await getAnswer(chat_id);
+            if (answer) {
+              this.messages.push({
+                id: this.messageId++,
+                text: answer || "Ответ не найден.",
+                isUser: false,
+              });
+            } else {
+              this.messages.push({
+                id: this.messageId++,
+                text: "Ответ не был получен.",
+                isUser: false,
+              });
             }
-          ).then().finally(this.visibleGenMessage = true);
-          await axios.post(
-              "http://81.94.159.227:1203/api/v1/chat/message/clear",null,
-              {
-                params: {
-                  chat_id: "1ca42f7e-0f47-4ffe-ae96-41988848997a",
-                }
-              }
-          )
-
-
-          // Добавляем сообщение ассистента
-          const assistantMessage = response.data.find(
-            (msg) => msg.role === "assistant"
-          );
-          this.messages.push({
-            id: this.messageId++,
-            text: assistantMessage?.content || "Ответ не найден.",
-            isUser: false,
-          });
-
-          this.saveSession();
+          } else {
+            this.messages.push({
+              id: this.messageId++,
+              text: "Не удалось найти или создать чат.",
+              isUser: false,
+            });
+          }
         } catch (error) {
-          console.error("Ошибка при отправке сообщения:", error);
+          console.error("Ошибка при обработке сообщения:", error);
           this.messages.push({
             id: this.messageId++,
             text: "Произошла ошибка. Попробуйте позже.",
@@ -152,6 +170,13 @@ export default {
           isUser: false,
         });
       }
+    },
+    clearChat() {
+      // Очищаем массив сообщений и сбрасываем счетчик ID
+      this.messages = [];
+      this.messageId = 0;
+      localStorage.removeItem("chatSession");
+      this.scrollToBottom();
     },
   },
   mounted() {
@@ -212,4 +237,37 @@ mark {
   border-radius: 50%;
   box-shadow: 0 1rem 2.2rem rgba(0, 0, 0, 0.2);
 }
+/* From Uiverse.io by vinodjangid07 */
+.bin-button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 55px;
+  height: 55px;
+  border-radius: 15px;
+  background-color: rgb(255, 95, 95);
+  cursor: pointer;
+  border: 3px solid rgb(255, 201, 201);
+  transition-duration: 0.3s;
+}
+.bin-bottom {
+  width: 15px;
+}
+.bin-top {
+  width: 17px;
+  transform-origin: right;
+  transition-duration: 0.3s;
+}
+.bin-button:hover .bin-top {
+  transform: rotate(45deg);
+}
+.bin-button:hover {
+  background-color: rgb(255, 0, 0);
+}
+.bin-button:active {
+  transform: scale(0.9);
+}
+
+
 </style>
